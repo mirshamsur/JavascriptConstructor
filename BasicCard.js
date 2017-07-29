5ca1943c9f2eca325e87c79d215cecb137c2dc85
Shamsur Mir <shamsurmir@gmail.com>
2017-07-29 13:32:43 -0400

    changes to file

--- BasicCard.js
+++ BasicCard.js
@@ -4,30 +4,28 @@ function BasicCard(front, back) {
 	this.back = back;
 }
 
-function ClozeCard(text, cloze) {
-  this.text = text;
-  this.cloze = cloze;
 
-// function ClozeCard (partial, cloze){
-// 	this.partialText = partial;
-// 	this.cloze = cloze;
-// }
+function ClozeCard (partial, cloze){
+	this.partialText = partial;
+	this.cloze = cloze;
+   this.fullText = function() {
+    return this.cloze + this.partialText;
+  }
+
+}
 
 // }
 	// this.partialText = function() {
  //    return this.text + this.cloze;
  //  }
- function ClozeCard (partial, cloze){
-	this.partialText = text;
-	this.patial = this.cloze;
-}
+//  function ClozeCard (partial, cloze){
+// 	this.partialText = partial;
+// 	this.partial = this.cloze;
+// }
 
 
-  this.fullText = function() {
-    return this.text + this.cloze;
-  }
+ 
 
-}
 var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
 
@@ -38,12 +36,12 @@ var firstPresident = new BasicCard(
 
 
 var firstPresidentCloze = new ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
+    "............ was the first president of the United States.", "George Washington");
 
 console.log(firstPresidentCloze.cloze);
 
 console.log(firstPresidentCloze.partialText);
 
-console.log(firstPresidentCloze.fullText);
+console.log(firstPresidentCloze.fullText());
 
 
